72e7cb8b1425c2e1df4a32e04189fe5cbeec488f
Warish <106527502+warination@users.noreply.github.com>
2022-10-18 14:57:58 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,7 +1,16 @@
+let toggleBtn = document.querySelector('.toggleBtn');
+
 function loadCoupan(){
     document.getElementById('coupan-container').style.visibility = 'visible';
 }
 
 function closeCoupan(){
     document.getElementById('coupan-container').style.visibility = 'hidden';
+}
+
+
+function theme(){
+    let mybody = document.querySelector('body');
+
+    mybody.classList.toggle('light')
 }
